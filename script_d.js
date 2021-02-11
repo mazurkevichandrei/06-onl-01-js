2e60c22652104a9f17eae28f7c57a1bd5986272a
mazurkevichandrei <mazurkevi4@gmail.com>
2021-02-11 12:41:17 +0300

    2D-1

--- script_d.js
+++ script_d.js
@@ -1,3 +1,8 @@
 let customerAge=prompt("Сколько вам лет?");
 
 alert("Поздравляем, Вам "+customerAge+" полных лет!")
+
+if (customerAge >= 18) 
+    alert("Вам больше 18 лет")
+else if (customerAge < 18) 
+    alert("Вам нет 18 лет!")  
