97ea47fd695b8f39390636f2c2fef4cba4e5b7dd
mazurkevichandrei <mazurkevi4@gmail.com>
2021-02-11 13:41:49 +0300

    2B-1

--- script_b.js
+++ script_b.js
@@ -1,3 +1,15 @@
-let customerAge=prompt("Сколько вам лет?");
+let a;
+let b = true;
+let c = 5;
+let d = 'str value';
+let e = 1n;
+let f = Symbol("sym");
+let obj = {key_1: "Value_1", key_2: "Value_2"};
 
-alert("Поздравляем, Вам "+customerAge+" полных лет!")
+alert(a)
+alert(b)
+alert(c)
+alert(d)
+alert(e)
+alert(f.toString())
+console.log(obj)
