fff492d29617d149995423e0f2bddfd1635721fb
Andrew Mazurkevich <76482901+mazurkevichandrei@users.noreply.github.com>
2021-03-01 17:08:31 +0300

    Add files via upload

--- MyFigure.js
+++ MyFigure.js
@@ -19,6 +19,23 @@ class Figure{
     }
     return coorForShow
   }
+  calcSquare(){
+    const arrLen=this.coordinates.length
+    let f=this.coordinates[arrLen-2]*this.coordinates[1]
+    let s=this.coordinates[0]*this.coordinates[arrLen-1]
+    let fpart=0
+    let spart=0
+    for(let i=0;i<arrLen-2;i+=2){
+      fpart+=this.coordinates[i]*this.coordinates[i+3]
+      spart+=this.coordinates[i+2]*this.coordinates[i+1]
+    }
+    // console.log(fpart)
+    // console.log(spart)
+    // console.log(f)
+    // console.log(s)
+    let figureSquare=0.5*((f+fpart)-(s+spart))
+    console.log('Square of Figure: '+Math.abs(figureSquare))
+  }
 }
 //-------------CIRCLE CLASS
 class Circle extends Figure{
@@ -63,11 +80,12 @@ class Quadrate extends Circle{
     }
 }
 
-const fig = new Figure([2,2,5,5,4,4,8,8])
-const cir = new Circle([5,5],20)
-const rec = new Rectangle([7,7],10,20)
+const fig = new Figure([1,7,6,6,7,7,8,15,9,0,15,325])
+const cir = new Circle([5,5],10)
+const rec = new Rectangle([7,7],40,20)
 const squ = new Quadrate([8,8],15)
 console.log('Figure: '+fig.showCoordinates())
+fig.calcSquare()
 console.log('Circle: '+cir.showCoordinates())
 cir.calcSquare()
 console.log('Rectangle: '+rec.showCoordinates())
