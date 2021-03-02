28b210f4a0df258328ca9fa20406eb138b7c8831
Andrew Mazurkevich <76482901+mazurkevichandrei@users.noreply.github.com>
2021-03-02 13:46:27 +0300

    Add files via upload

---
+++ modal/script.js
@@ -0,0 +1,35 @@
+//ADD USER BUTTON
+const btnAddUser = document.querySelector('.add-user');
+//FORM FOR INPUT
+const form = document.querySelector('.modal');
+//FORM FIELDS
+const inpt = document.querySelectorAll('input')
+//FORM CLOSE BUTTONS
+const btnClose = document.querySelectorAll('.close');
+
+//Action Show_element
+function showElement (elm){
+    elm.style.display='block'    
+}
+//Action Hide_element
+function hideElement(elm){
+    elm.style.display='none'
+}
+
+//Scenario for ADD_USER Button
+function btnAddUserAction(){
+    showElement(form)
+}
+//Scenario for CLOSE Buttons
+function btnCloseAction(){
+    hideElement(form)
+    for(let item of inpt){
+        item.value=('Empty for Test')
+    }
+}
+
+//Listeners for Buttons
+btnAddUser.addEventListener('click',btnAddUserAction)
+for(let item of btnClose){
+    item.addEventListener('click', btnCloseAction)
+}
