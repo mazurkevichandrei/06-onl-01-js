82aad11189f9a66f1490d4c05a5a12a33eb48497
Andrew Mazurkevich <76482901+mazurkevichandrei@users.noreply.github.com>
2021-03-08 17:13:17 +0300

    Update 5-2_-_Calculator.js

--- 5-2_-_Calculator.js
+++ 5-2_-_Calculator.js
@@ -1,6 +1,6 @@
-var arr1 = [1,2,3,10,'n',1,'num'] //expected 17
-var arr2 = [1,2,'3'] //expected 3
-var arr3 = ['str','10',1,5,0] //expected 6
+let arr1 = [1,2,3,10,'n',1,'num'] //expected 17
+let arr2 = [1,2,'3'] //expected 3
+let arr3 = ['str','10',1,5,0] //expected 6
 
 function calc (data){
   let total = 0
@@ -12,6 +12,6 @@ function calc (data){
   return('Total: ' + total)
 }
 
-calc(arr1)
-calc(arr2)
-calc(arr3)
+console.log(calc(arr1))
+console.log(calc(arr2))
+console.log(calc(arr3))
