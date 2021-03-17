2adf1ae0b774b7042ecf23c3f2cdc68f35ae27a2
Andrew Mazurkevich <76482901+mazurkevichandrei@users.noreply.github.com>
2021-03-17 16:10:00 +0300

    Update script.js

--- Lesson_9_Tabs/script.js
+++ Lesson_9_Tabs/script.js
@@ -3,28 +3,28 @@ const tabButtonActive = document.querySelector('.bookmark_tab.bookmark_tab_activ
 const contentItem = document.querySelectorAll('.content_item');
 const delTabBtn = document.querySelectorAll('.delete_tab_btn');
 
-function tabButtonSwitcher(evt){
+function tabButtonSwitcher(evt) {
     const clickedButton = evt.target
     const tabButtonActive = document.querySelector('.bookmark_tab.bookmark_tab_active');
     const contentItemActive = document.querySelector('.content_item.content_item_active');
-    const linkId = clickedButton.getAttribute('tabid')
-    const newItemActive = document.querySelector(`.content_item[tabid='${linkId}']`)
-    
+    const content = document.querySelector('.content');
+    const linkId = clickedButton.dataset.tabid
+    const newItemActive = content.querySelector(`[data-tabid='${linkId}']`)
     //Change Buttons Classes:
     tabButtonActive.classList.remove('bookmark_tab_active')
     clickedButton.classList.add('bookmark_tab_active')
-    
+
     //Change Content classes:
     contentItemActive.classList.remove('content_item_active')
     newItemActive.classList.add('content_item_active')
 }
 
-function delTab(evt){
+function delTab(evt) {
     const clickedDelButton = evt.target
     const elementToDel = clickedDelButton.parentElement
     const contentItemActive = document.querySelector('.content_item.content_item_active');
 
-    if(elementToDel.classList.contains('bookmark_tab_active')){
+    if (elementToDel.classList.contains('bookmark_tab_active')) {
         elementToDel.classList.remove('bookmark_tab_active')
         elementToDel.remove()
         const tabButton = document.querySelectorAll('.bookmark_tab');
@@ -32,10 +32,11 @@ function delTab(evt){
         contentItemActive.remove()
         const contentItem = document.querySelectorAll('.content_item');
         contentItem[0].classList.add('content_item_active')
-    }
-    else{
-        const linkId = clickedDelButton.parentElement.getAttribute('tabid')
-        const contentToDel = document.querySelector(`.content_item[tabid='${linkId}']`)
+    } else {
+        const content = document.querySelector('.content');
+        const clickedButton = evt.target
+        const linkId = clickedButton.parentElement.dataset.tabid
+        const contentToDel = content.querySelector(`[data-tabid='${linkId}']`)
         elementToDel.remove()
         contentToDel.remove()
     }
