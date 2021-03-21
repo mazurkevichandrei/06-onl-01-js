03f4e481130734aa399b0f49af2989a72e43dbce
Andrew Mazurkevich <76482901+mazurkevichandrei@users.noreply.github.com>
2021-03-21 13:11:52 +0300

    Update Lesson_12_Horses.js

--- Lesson_12_Horses.js
+++ Lesson_12_Horses.js
@@ -1,18 +1,18 @@
-function randTime() {
+function randomTime() {
     let time = Math.ceil(Math.random() * 5)
     return time
 }
-function horse(name, func) {
+function createHorse(name, timeFunc) {
     let myPromise = new Promise(function(res, rej) {
         setTimeout(function() {
-            res(`Horse ${name} finished in ${func()} seconds`)
+            res(`Horse ${name} finished in ${timeFunc()} seconds`)
         }, 2000)
     })
     return myPromise
 }
 let horses = ['Tim', 'Nik', 'Kim']
 for (i = 0; i < horses.length; i++) {
-    let newHorse = horse(horses[i], randTime);
+    let newHorse = createHorse(horses[i], randomTime);
     newHorse.then(function(data) {
         console.log(data)
     })
